e440c748f6a55d2827cee53f5ea92f4e65b2212f
ManoloiuAlexandru <manalexio@yahoo.com>
2026-03-18 20:39:20 +0200

    Initial commit

---
+++ python/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from utils.classes import User
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Java and Python application"}
+
+
+@app.post("/add_user")
+def add_user(user: User):
+    print(user.name)
+    print(user.password)
+    return {"result": "bau"}
